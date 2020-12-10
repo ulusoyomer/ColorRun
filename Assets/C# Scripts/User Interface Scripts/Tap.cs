daf6629830280c16f40bc294132f7893abda0761
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2020-12-10 22:31:52 +0300

    Create Folders
    Create Folders And Start

---
+++ Assets/C# Scripts/User Interface Scripts/Tap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Tap : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private static bool isTapped;
+    public static bool GetIsTapped()
+    {
+        return isTapped;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTapped = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTapped = false;
+    }
+}
